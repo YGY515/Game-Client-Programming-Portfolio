a1ccd4910311e2963cdb0cae6e77161256667cbe
YGY 윤가영 <107830010+YGY515@users.noreply.github.com>
2026-02-18 13:45:32 +0900

    WeaponManager.cs 코드 가독성 수정

--- 프로젝트 샘플 코드/코드: 안티데시스 리빌드/Weapon/WeaponManager.cs
+++ 프로젝트 샘플 코드/코드: 안티데시스 리빌드/Weapon/WeaponManager.cs
@@ -1,38 +1,33 @@
 using UnityEngine;
 
-
 public class WeaponManager : MonoBehaviour
 {
     public WeaponData weaponData;
     public GameObject[] weaponObjects;
     private IWeapon currentWeapon;
 
-    
     void Start()
     {
         currentWeapon = weaponObjects[weaponData.weapon_index].GetComponent<IWeapon>();
 
         if (currentWeapon == null)
-            Debug.LogError($"{weaponObjects[weaponData.weapon_index].name} 오브젝트에 IWeapon 컴포넌트가 없음");
+            Debug.LogError("Need to assign currentWeapon by using weaponObjects");
 
         weaponObjects[weaponData.weapon_index].SetActive(false);
-
     }
-
 
     public void UseWeapon()
     {
-        
         if (currentWeapon != null)
             currentWeapon.Attack();
         else
-            Debug.LogError("currentWeapon에 아직 할당되지 않음");
+            Debug.LogError("Need to assign currentWeapon");
     }
 
     public void ChangeWeapon()
     {
         weaponData.weapon_index = (weaponData.weapon_index + 1) % weaponObjects.Length;
         currentWeapon = weaponObjects[weaponData.weapon_index].GetComponent<IWeapon>();
-
     }
 }
+
