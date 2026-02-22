efd87ef7256b4c5d9bc8b837cd972af2accf4802
YGY 윤가영 <107830010+YGY515@users.noreply.github.com>
2026-02-22 15:16:35 +0900

    PlayerController.cs 코드 가독성 수정

--- 프로젝트 샘플 코드/코드: 안티데시스 리빌드/Controller/PlayerController.cs
+++ 프로젝트 샘플 코드/코드: 안티데시스 리빌드/Controller/PlayerController.cs
@@ -9,6 +9,12 @@ public class PlayerController : MonoBehaviour
     public WeaponManager weaponManager;
     public WeaponData weaponData;
 
+    public AudioSource weapon_ChangeSource;
+    public AudioClip Clip;
+    private bool canAttack = true;
+    private bool canDash = true;
+    private bool isDashing = false;
+
     [SerializeField] private BossHealth BossStatus;
     
     [SerializeField] private GameObject Baton_Icon;
@@ -20,19 +26,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject Dash_Icon;
     [SerializeField] private GameObject Dash_Empty;
 
-    public AudioSource weapon_ChangeSource;
-    public AudioClip Clip;
-
-    private bool canAttack = true;
-    private bool canDash = true;
-    private bool isDashing = false;
-
-
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
+    
     void Update()
     {
         HandleMovementInput();
@@ -51,11 +50,11 @@ public class PlayerController : MonoBehaviour
         Vector2 movement = new Vector2(horizontal, vertical).normalized * playerData.moveSpeed;
         rb.velocity = movement;
 
-
         if (movement != Vector2.zero)
         {
             anim.SetBool("Running", true);
-            if (!isDashing) // ´ë½¬ ÁßÀÌ ¾Æ´Ò ¶§¸¸ ¼Óµµ º¯°æ
+            
+            if (!isDashing) // ëŒ€ì‰¬ ì¤‘ì´ ì•„ë‹ ë•Œë§Œ ì†ë„ ë³€ê²½
             {
                 if (Input.GetKey(KeyCode.LeftShift))
                 {
@@ -68,38 +67,30 @@ public class PlayerController : MonoBehaviour
                     playerData.moveSpeed  = 5f;
                 }
             }
- 
 
-            if (horizontal > 0)     // ¿À¸¥ÂÊ
+            if (horizontal > 0)      // ì˜¤ë¥¸ìª½
             {
                 anim.SetFloat("Looking", 0.66f);
-
             }
-            else if (horizontal < 0) // ¿ÞÂÊ
+            else if (horizontal < 0) // ì™¼ìª½
             {
-
                 anim.SetFloat("Looking", 0.33f);
-
             }
-            else if (vertical > 0)  // À§ÂÊ
+            else if (vertical > 0)   // ìœ„ìª½
             {
                 anim.SetFloat("Looking", 1.00f);
-
             }
-            else if (vertical < 0) // ¾Æ·¡ÂÊ
+            else if (vertical < 0)   // ì•„ëž˜ìª½
             {
                 anim.SetFloat("Looking", 0.00f);
-
             }
-
         }
+    
         else
         {
             anim.SetBool("Flying", false);
             anim.SetBool("Running", false);
-
         }
-
     }
 
     void WeaponInput()
@@ -127,14 +118,13 @@ public class PlayerController : MonoBehaviour
                 }
             }
         }
-            
     }
 
     private IEnumerator WeaponCooldown()
     {
         if (weaponData.weapon_index == 0)
         {   
-            // ±ÙÁ¢ ¹«±âÀÏ ¶§
+            // ê·¼ì ‘ ë¬´ê¸°ì¼ ë•Œ
             canAttack = false;
             if (Baton_Icon != null) Baton_Icon.SetActive(false);
             if (Baton_Empty != null) Baton_Empty.SetActive(true);
@@ -148,7 +138,7 @@ public class PlayerController : MonoBehaviour
 
         else if (weaponData.weapon_index == 1)
         {   
-            // ¿ø°Å¸® ¹«±âÀÏ ¶§
+            // ì›ê±°ë¦¬ ë¬´ê¸°ì¼ ë•Œ
             canAttack = false;
             if (Gun_Icon != null) Gun_Icon.SetActive(false);
             if (Gun_Empty != null) Gun_Empty.SetActive(true);
@@ -178,17 +168,14 @@ public class PlayerController : MonoBehaviour
 
         float prevSpeed = playerData.moveSpeed;
         playerData.moveSpeed += playerData.dashSpeed;
-
         yield return new WaitForSeconds(playerData.dashDuration);
 
         playerData.moveSpeed = prevSpeed;
         isDashing = false;
-
         yield return new WaitForSeconds(playerData.weaponCooldown);
 
         if (Dash_Icon != null) Dash_Icon.SetActive(true);
         if (Dash_Empty != null) Dash_Empty.SetActive(false);
         canDash = true;
     }
-
 }
