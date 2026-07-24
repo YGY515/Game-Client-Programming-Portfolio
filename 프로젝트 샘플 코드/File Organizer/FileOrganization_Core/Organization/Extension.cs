98fc6fb6cbee67f41deea8a3442a0e9affeec9b0
YGY 윤가영 <107830010+YGY515@users.noreply.github.com>
2026-07-24 13:05:31 +0900

    확장자에 따른 병렬 처리로 수정

--- 프로젝트 샘플 코드/File Organizer/FileOrganization_Core/Organization/Extension.cs
+++ 프로젝트 샘플 코드/File Organizer/FileOrganization_Core/Organization/Extension.cs
@@ -1,14 +1,16 @@
-﻿namespace FileOrganization_Core.Organization
+namespace FileOrganization_Core.Organization
 {
     public class Extension : FileOrganizerBase
     {
         string _path = null;
         int count = 0;
         HashSet<string> fileList = new HashSet<string>();
+        List<String> files = new List<string>();
 
         public override string Organize(string path)
         {
-           _path = path;
+            _path = path;
+            files = Directory.GetFiles(_path).ToList();
 
             CollectFiles();
             CreateFolders();
@@ -19,7 +21,7 @@
 
         public override void CollectFiles()
         {
-            foreach (var file in Directory.GetFiles(_path))
+            foreach (var file in files)
             {
                 count++;
                 string extension = Path.GetExtension(file);
@@ -38,14 +40,25 @@
 
         public override void MoveFiles()
         {
-            foreach (var file in Directory.GetFiles(_path))
-            {
-                string extension = Path.GetExtension(file).Replace(".", "");
-                string destFolder = Path.Combine(_path, extension);
-                string destPath = Path.Combine(destFolder, Path.GetFileName(file));
+            SemaphoreSlim semaphore = new SemaphoreSlim(4);
 
-                File.Move(file, destPath);
-            }
+            Parallel.ForEach(files, file =>
+            {
+                semaphore.Wait();
+                try
+                {
+                    var info = new FileInfo(file);
+                    string extension = Path.GetExtension(file).Replace(".", "");
+                    string destFolder = Path.Combine(_path, extension);
+                    string destPath = Path.Combine(destFolder, Path.GetFileName(file));
+
+                    File.Move(file, destPath);
+                }
+                finally
+                {
+                    semaphore.Release();
+                }
+            });
         }
     }
 }
