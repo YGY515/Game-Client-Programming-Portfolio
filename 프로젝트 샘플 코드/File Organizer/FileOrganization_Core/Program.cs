12ed2fb72495b3d918804e61c3eaa404b241eee3
YGY 윤가영 <107830010+YGY515@users.noreply.github.com>
2026-07-24 13:03:35 +0900

    다중 폴더 정리 가능하도록 변경

--- 프로젝트 샘플 코드/File Organizer/FileOrganization_Core/Program.cs
+++ 프로젝트 샘플 코드/File Organizer/FileOrganization_Core/Program.cs
@@ -1,4 +1,4 @@
-﻿using FileOrganization_Core;
+using FileOrganization_Core;
 using FileOrganization_Core.Organization;
 
 class Program
@@ -28,27 +28,26 @@ class Program
         }
 
         Console.WriteLine();
-        Console.WriteLine("폴더를 정리할 기준을 골라주세요. 확장자 or 날짜 or 파일명 언어?");
+        Console.WriteLine("폴더를 정리할 기준을 골라주세요. 확장자 / 날짜 / 언어");
         Console.WriteLine("날짜의 경우, YYYY-MM 기준으로 정리됩니다.");
-        Console.WriteLine("파일명 언어의 경우, 한글이면 Korean, 영어면 English로 분류됩니다.");
+        Console.WriteLine("언어의 경우, 파일 이름이 한글이면 Korean, 영어면 English로 분류됩니다.");
         string mode = Console.ReadLine();
 
-        switch (mode)
+        var folders = new List<string>() { path };    // 입력한 경로의 모든 폴더
+        folders.AddRange(Directory.GetDirectories(path));
+
+        Parallel.ForEach(folders, folder =>
         {
-            case "확장자":
-                fileOrganizer = new Extension();
-                break;
-
-            case "날짜":
-                fileOrganizer = new Date();
-                break;
-
-            case "파일명 언어":
-                fileOrganizer = new Language();
-                break;
-        }
-
-        string result = fileOrganizer.Organize(path);
-        Console.WriteLine(result);
+            // 폴더마다 새 인스턴스 생성
+            FileOrganizerBase organizer = mode switch
+            {
+                "확장자" => new Extension(),
+                "날짜" => new Date(),
+                "언어" => new Language(),
+                _ => throw new NotImplementedException()
+            };
+            string result = organizer.Organize(folder);
+            Console.WriteLine($"[{folder}] {result}");
+        });
     }
 }
