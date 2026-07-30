67efb2bcb6be6d925980fa22ff7c450f2f20fe45
YGY 윤가영 <107830010+YGY515@users.noreply.github.com>
2026-07-30 17:10:23 +0900

    ASP.NET Core Web API을 활용한 DB 구축

---
+++ 프로젝트 샘플 코드/File Organizer/FileOrganization_Api/FileOrganization_Api/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using FileOrganization_Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+namespace FileOrganization_Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<OrganizeLog> OrganizeLogs { get; set; }
+    }
+}
---
+++ 프로젝트 샘플 코드/File Organizer/FileOrganization_Api/FileOrganization_Api/LogsController.cs
@@ -0,0 +1,23 @@
+﻿using FileOrganization_Api.Data;
+using FileOrganization_Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileOrganization_Api.Controllers
+{
+    [Route("api/logs")]
+    [ApiController]
+    public class LogsController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public LogsController(AppDbContext db) { _db = db; }
+
+        [HttpPost]
+        public async Task<IActionResult> PostLog(OrganizeLog log)
+        {
+            _db.OrganizeLogs.Add(log);
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
