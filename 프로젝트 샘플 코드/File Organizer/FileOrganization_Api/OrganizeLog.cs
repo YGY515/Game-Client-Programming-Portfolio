5bfd323cc1d62f88fe16844ee19b13b5c565ee9c
YGY 윤가영 <107830010+YGY515@users.noreply.github.com>
2026-07-30 17:15:47 +0900

    ASP.NET Core Web API을 활용한 DB 연동 기능 구축

---
+++ 프로젝트 샘플 코드/File Organizer/FileOrganization_Api/OrganizeLog.cs
@@ -0,0 +1,14 @@
+﻿namespace FileOrganization_Api.Models
+{
+    public class OrganizeLog
+    {
+        public int Id { get; set; }          
+        public DateTime Date { get; set; }
+        public string Path { get; set; }
+
+        public int OrganizeMethod { get; set; }
+        public int FileCount { get; set; }
+        public int FolderCount { get; set; }
+        public bool WasCancelled { get; set; }
+    }
+}
